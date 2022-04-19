a909d7d2563bc8e3413408b7f8c17c8470ddb2e3
Guilherme Novaes <gui.novaes94@outlook.com>
2022-04-19 09:43:58 -0300

    Update _3d.py

--- keras_resnet/models/_3d.py
+++ keras_resnet/models/_3d.py
@@ -103,13 +103,13 @@ class ResNet3D(keras.Model):
             
             x = keras.layers.GlobalAveragePooling3D(name="pool5")(x)
             if(include_top == 2):
+                if(dropout > 0):
+                    x = keras.layers.Dropout(dropout)(x)
                 if(classes == 1):
                     activation='sigmoid'
                 else:
                     activation='softmax'
                 x = keras.layers.Dense(classes, activation=activation, name="fc%d"%classes)(x)
-                if(dropout > 0):
-                    x = keras.layers.Dropout(dropout)(x)
 
             super(ResNet3D, self).__init__(inputs=inputs, outputs=x, *args, **kwargs)
         else:
